cd2409a31f5ee25a8f9fa97c67dc3515f330ab8c
akradin <alex.kradin@gmail.com>
2017-02-28 14:29:27 -0500

    Consolidate scroll functions into on JS file
    Added different font colors to header and nav bar, add github
    and email links to contact section. Added two more photos and now
    all photo titles are links to each photo.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const $ = require('jQuery');
+// const $ = require('jQuery');
 
 let topScroll = () => {
   // scroll to top
@@ -12,11 +12,28 @@ let topScroll = () => {
      $('html, body').stop().animate({
          'scrollTop': $target.offset().top
       }, 1100, 'swing', function () {
-         $(window).location.hash = target;
+         window.location.hash = target;
      });
    });
 };
 
+let contactScroll = () => {
+  // scroll to contact-section
+ $('a[href="#contact-container"]').on('click',function (e) {
+   e.preventDefault();
+
+   var target = this.hash;
+   var $target = $('#contact-container');
+
+   $('html, body').stop().animate({
+       'scrollTop': $target.offset().top
+    }, 1100, 'swing', function () {
+       window.location.hash = target;
+   });
+ });
+}
+
 $(()=>{
   topScroll();
+  contactScroll();
 });
